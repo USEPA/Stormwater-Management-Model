e849b2b83ff901ec0bd1c9aef2f2efedb220eb3b
Bryant E. McDonnell <bemcdonnell@gmail.com>
2016-08-31 23:03:02 -0400

    Added more getter functions

--- src/inputAPI.c
+++ src/inputAPI.c
@@ -26,6 +26,7 @@
 
 #include "swmm5.h"                     // declaration of exportable functions
 #include "inputAPI.h"
+#include "hash.h"
 
 int DLLEXPORT  swmm_countObjects(int type, int *count)
 //
@@ -67,7 +68,7 @@ int DLLEXPORT swmm_getObjectId(int type, int index, char *id)
 		return(errorcode);
 	}
 	
-	if (index < 0 || index >= Nobjects[type]) return(99922);
+	if (index < 0 || index >= Nobjects[type]) return(902);
 	switch (type)
 	{
 		case GAGE:
@@ -107,44 +108,165 @@ int DLLEXPORT swmm_getObjectId(int type, int index, char *id)
    return(errorcode);
 }
 
+int DLLEXPORT swmm_getNodeType(int index, int *Ntype)
+//
+// Input: 	index = Index of desired ID
+//			Ntype = Node type (Based on enum NodeType)
+// Output: 	returns API Error
+// Purpose: Gets Node Type
+{
+	if (index < 0 || index >= Nobjects[NODE]) return(902);
+	*Ntype = Node[index].type;
+	return(0);
+}
 
-int DLLEXPORT swmm_getNodeParam(int index, int Param, float *value)
+int DLLEXPORT swmm_getLinkType(int index, int *Ltype)
+//
+// Input: 	index = Index of desired ID
+//			Ltype = Link type (Based on enum NodeType)
+// Output: 	returns API Error
+// Purpose: Gets Link Type
+{
+	if (index < 0 || index >= Nobjects[LINK]) return(902);
+	*Ltype = Link[index].type;
+	return(0);
+}
+
+int DLLEXPORT swmm_getLinkConnections(int index, int *Node1, int *Node2)
 //
 // Input: 	type = object type (Based on ObjectType enum)
 //			index = Index of desired ID
+// Output: 	Node1 and Node2
+// Purpose: Gets link Connection ID Indeces
+{
+	if (index < 0 || index >= Nobjects[LINK]) return(902);
+	
+	*Node1 = Link[index].node1;
+	*Node2 = Link[index].node2;
+	
+	return(0);
+}
+
+int DLLEXPORT swmm_getNodeParam(int index, int Param, double *value)
+//
+// Input: 	index = Index of desired ID
+//			param = Parameter desired (Perhaps define enum )
 //			id = pointer to id pass by reference
 // Output: 	returns API Error
-// Purpose: Gets ID for any object
+// Purpose: Gets Node Parameter
 {
 	// Add Checks later on (08/31/2016 ** BEM)
 	// Add Enum later for values
-	if (index < 0 || index >= Nobjects[NODE]) return(999232);
+	if (index < 0 || index >= Nobjects[NODE]) return(902);
 	
 	switch(Param)
 	{
-		// Type
-		case 0: *value = Node[index].type; break;
 		// invertElev
-		case 1: *value = Node[index].invertElev; break;			
+		case 0: *value = Node[index].invertElev; break;			
 		// fullDepth
-		case 2: *value = Node[index].fullDepth; break;		
+		case 1: *value = Node[index].fullDepth; break;		
 		// surDepth
-		case 3: *value = Node[index].surDepth; break;	
+		case 2: *value = Node[index].surDepth; break;	
 		// pondedArea
-		case 4: *value = Node[index].pondedArea; break;	
+		case 3: *value = Node[index].pondedArea; break;	
 		// initDepth
-		case 5: *value = Node[index].initDepth; break;					
+		case 4: *value = Node[index].initDepth; break;					
 	}
 	return(0);
 }
 
 
+int DLLEXPORT swmm_getLinkParam(int index, int Param, double *value)
+//
+// Input: 	index = Index of desired ID
+//			param = Parameter desired (Perhaps define enum )			
+//			id = pointer to id pass by reference
+// Output: 	returns API Error
+// Purpose: Gets Link Parameter
+{
+	// Add Checks later on (08/31/2016 ** BEM)
+	// Add Enum later for values
+	if (index < 0 || index >= Nobjects[LINK]) return(902);
+	
+	switch(Param)
+	{
+		// offset1
+		case 0: *value = Link[index].offset1; break;			
+		// offset2
+		case 1: *value = Link[index].offset2; break;		
+		// q0
+		case 2: *value = Link[index].q0; break;	
+		// qLimit
+		case 3: *value = Link[index].qLimit; break;	
+		// cLossInlet
+		case 4: *value = Link[index].cLossInlet; break;		
+		// cLossOutlet
+		case 5: *value = Link[index].cLossOutlet; break;		
+		// cLossAvg
+		case 6: *value = Link[index].cLossAvg; break;		
+		// seepRate
+		case 7: *value = Link[index].seepRate; break;				
+	}
+	return(0);
+}
 
+int DLLEXPORT swmm_getSubcatchParam(int index, int Param, double *value)
+//
+// Input: 	index = Index of desired ID
+//			param = Parameter desired (Perhaps define enum )
+//			id = pointer to id pass by reference
+// Output: 	returns API Error
+// Purpose: Gets Subcatchment Parameter
+{
+	// Add Checks later on (08/31/2016 ** BEM)
+	// Add Enum later for values
+	if (index < 0 || index >= Nobjects[SUBCATCH]) return(902);
+	
+	switch(Param)
+	{
+		// width
+		case 0: *value = Subcatch[index].width; break;			
+		// area
+		case 1: *value = Subcatch[index].area; break;		
+		// fracImperv
+		case 2: *value = Subcatch[index].fracImperv; break;	
+		// slope
+		case 3: *value = Subcatch[index].slope; break;	
+		// curbLength
+		case 4: *value = Subcatch[index].curbLength; break;		
+		// initBuildup
+		//case 5: *value = Subcatch[index].initBuildup; break;					
+	}
+	return(0);	
+}
 
-
-
-
-
+int DLLEXPORT swmm_getSubcatchOutConnection(int index, int *type, int *Index )
+//
+// Input: 	type = object type (Based on ObjectType enum) (Subcatchments can load to Node or another Subcatchment)
+//			index = Index of desired ID
+// Output: 	Node1 and Node2
+// Purpose: Gets Subcatchment Connection ID Indeces for either Node or Subcatchment
+{
+	if (index < 0 || index >= Nobjects[SUBCATCH]) return(902);
+	
+	if (Subcatch[index].outNode == -1 && Subcatch[index].outSubcatch == -1)
+	{
+		*Index = index; // Case of self Loading subcatchment
+		*type = SUBCATCH;
+	}
+	if (Subcatch[index].outNode >= 0)
+	{
+		*Index = Subcatch[index].outNode;
+		*type = NODE;
+	}
+	if (Subcatch[index].outSubcatch >= 0)
+	{
+		*Index = Subcatch[index].outSubcatch;
+		*type = SUBCATCH;
+	}
+	
+	return(0);
+}
 
 
 
--- src/inputAPI.h
+++ src/inputAPI.h
@@ -29,8 +29,16 @@ extern "C" {
 // Input API Exportable Functions
 int DLLEXPORT  swmm_countObjects(int type, int *count);
 int DLLEXPORT  swmm_getObjectId(int type, int index, char *id);
-int DLLEXPORT  swmm_getNodeParam(int index, int Param, float *value);
 
+int DLLEXPORT  swmm_getNodeType(int index, int *Ntype);
+int DLLEXPORT  swmm_getLinkType(int index, int *Ltype);
+
+int DLLEXPORT  swmm_getLinkConnections(int index, int *Node1, int *Node2);
+int DLLEXPORT  swmm_getSubcatchOutConnection(int index, int *type, int *Index );
+
+int DLLEXPORT  swmm_getNodeParam(int index, int Param, double *value);
+int DLLEXPORT  swmm_getLinkParam(int index, int Param, double *value);
+int DLLEXPORT  swmm_getSubcatchParam(int index, int Param, double *value);
 
 #ifdef __cplusplus 
 }   // matches the linkage specification from above */ 
