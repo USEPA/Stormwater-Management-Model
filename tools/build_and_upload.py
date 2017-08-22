608dc6b8f505f87517bbca3e47bde49f5bd59ce9
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-08-22 16:00:39 -0400

    enable py34 proper testing and packaging

--- tools/build_and_upload.py
+++ tools/build_and_upload.py
@@ -11,12 +11,13 @@ def main():
     pr_number = os.environ.get('APPVEYOR_PULL_REQUEST_NUMBER')
     branch= os.environ.get('APPVEYOR_REPO_BRANCH')
     token = os.environ.get('SWWM_CI_UPLOAD_TOKEN')
+    python = os.environ.get('PYTHON_VERSION')
 
     print([pr_number, branch])
 
     if branch == 'develop' and pr_number is None:
         cmd = ['conda-build', 'conda.recipe', '--user', 'owa',
-               '--token', token, '--old-build-string']
+               '--token', token, '--old-build-string', '--python', python]
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
