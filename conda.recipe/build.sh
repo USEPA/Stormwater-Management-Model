e4b643f50dbadc658eea2ec8abf23c6039db5258
Bryant E. McDonnell <bemcdonnell@gmail.com>
2017-10-27 22:48:37 -0400

    CI Test

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,10 +4,6 @@
 mkdir build
 cd build
 
-# GCC Compatibiliy Issues
-conda uninstall gcc
-conda install -c serge-sans-paille gcc_49
-
 # Configure using the CMakeFiles
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE:STRING=Release ..
 
