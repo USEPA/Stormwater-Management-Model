9efebc94a1f9f2200f223668d74fa7b44dd1ccb2
Bryant E. McDonnell <bemcdonnell@gmail.com>
2017-10-27 22:35:00 -0400

    Test CI

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -4,10 +4,6 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
-# GCC Compatibiliy Issues
-conda uninstall gcc
-conda install -c serge-sans-paille gcc_49
-
 :: Configure using the CMakeFiles
 %LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release ..
 if errorlevel 1 exit 1
