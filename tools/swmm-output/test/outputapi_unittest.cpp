2372fa8abfd668459e7f1e141b1a0bd4d70163f5
Michael Tryby <tryby.michael@epa.gov>
2017-11-02 17:49:16 -0400

    Fixing indentation

--- tools/swmm-output/test/outputapi_unittest.cpp
+++ tools/swmm-output/test/outputapi_unittest.cpp
@@ -1,13 +1,13 @@
 /*
-*   outputapi_unittest.cpp
-*
-*   Author: Colleen Barr
-*   Modified by: Michael E. Tryby
-*
-*   Date: 11/2/2017
-*
-*   Unit testing for SWMM outputapi using Google Test.
-*/
+ *   outputapi_unittest.cpp
+ *
+ *   Author: Colleen Barr
+ *   Modified by: Michael E. Tryby
+ *
+ *   Date: 11/2/2017
+ *
+ *   Unit testing for SWMM outputapi using Google Test.
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -33,33 +33,33 @@ TEST(SMO_init, InitTest) {
 }
 
 TEST(SMO_open, OpenTest) {
-	std::string path = std::string(PROJECT_HOME) + std::string(DATA_PATH);
-	SMO_Handle p_handle = NULL;
-	SMO_init(&p_handle);
+    std::string path = std::string(PROJECT_HOME) + std::string(DATA_PATH);
+    SMO_Handle p_handle = NULL;
+    SMO_init(&p_handle);
 
-	int error = SMO_open(p_handle, path.c_str());
-	ASSERT_EQ(0, error);
+    int error = SMO_open(p_handle, path.c_str());
+    ASSERT_EQ(0, error);
     SMO_close(&p_handle);
 }
 
 class OutputapiTest : public testing::Test {
 protected:
-	// SetUp for OutputapiTest fixture
-	virtual void SetUp() {
-		std::string path = std::string(PROJECT_HOME) + std::string(DATA_PATH);
+    // SetUp for OutputapiTest fixture
+    virtual void SetUp() {
+        std::string path = std::string(PROJECT_HOME) + std::string(DATA_PATH);
 
-		error = SMO_init(&p_handle);
-		SMO_clearError(p_handle);
-		error = SMO_open(p_handle, path.c_str());
-	}
+        error = SMO_init(&p_handle);
+        SMO_clearError(p_handle);
+        error = SMO_open(p_handle, path.c_str());
+    }
 
-	// TearDown for OutputapiTest fixture
-	virtual void TearDown() {
-		SMO_free((void**)&array);
-		error = SMO_close(&p_handle);
-	}
+    // TearDown for OutputapiTest fixture
+    virtual void TearDown() {
+        SMO_free((void**)&array);
+        error = SMO_close(&p_handle);
+    }
 
-	int error = 0;
+    int error = 0;
     SMO_Handle p_handle = NULL;
 
     float* array = NULL;
@@ -67,84 +67,84 @@ protected:
 };
 
 TEST_F(OutputapiTest, getVersionTest) {
-	int version;
+    int version;
 
-	error = SMO_getVersion(p_handle, &version);
-	ASSERT_EQ(0, error);
+    error = SMO_getVersion(p_handle, &version);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(51000, version);
+    EXPECT_EQ(51000, version);
 }
 
 TEST_F(OutputapiTest, getProjectSizeTest) {
-	int* i_array = NULL;
-	// subcatchs, nodes, links, pollutants
-	int ref_array[4] = {8,14,13,2};
+    int* i_array = NULL;
+    // subcatchs, nodes, links, pollutants
+    int ref_array[4] = {8,14,13,2};
 
-	error = SMO_getProjectSize(p_handle, &i_array, &array_dim);
-	ASSERT_EQ(0, error);
+    error = SMO_getProjectSize(p_handle, &i_array, &array_dim);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(4, array_dim);
-	for (int i = 0; i < array_dim; i++)
-		EXPECT_EQ(ref_array[i], i_array[i]);
+    EXPECT_EQ(4, array_dim);
+    for (int i = 0; i < array_dim; i++)
+        EXPECT_EQ(ref_array[i], i_array[i]);
 
-	SMO_free((void**)&i_array);
+    SMO_free((void**)&i_array);
 }
 
 TEST_F(OutputapiTest, getFlowUnitsTest) {
-	int units = -1;
+    int units = -1;
 
-	error = SMO_getFlowUnits(p_handle, &units);
-	ASSERT_EQ(0, error);
-	EXPECT_EQ(0, units);
+    error = SMO_getFlowUnits(p_handle, &units);
+    ASSERT_EQ(0, error);
+    EXPECT_EQ(0, units);
 }
 
 TEST_F(OutputapiTest, getPollutantUnitsTest) {
-	int* i_array = NULL;
-	int ref_array[2] = {0, 1};
+    int* i_array = NULL;
+    int ref_array[2] = {0, 1};
 
-	error = SMO_getPollutantUnits(p_handle, &i_array, &array_dim);
-	ASSERT_EQ(0, error);
+    error = SMO_getPollutantUnits(p_handle, &i_array, &array_dim);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(2, array_dim);
-	for (int i = 0; i < array_dim; i++)
-		EXPECT_EQ(ref_array[i], i_array[i]);
+    EXPECT_EQ(2, array_dim);
+    for (int i = 0; i < array_dim; i++)
+        EXPECT_EQ(ref_array[i], i_array[i]);
 
-	SMO_free((void**)&i_array);
+    SMO_free((void**)&i_array);
 }
 
 TEST_F(OutputapiTest, getStartDateTest) {
-	double date = -1;
+    double date = -1;
 
-	error = SMO_getStartDate(p_handle, &date);
-	ASSERT_EQ(0, error);
+    error = SMO_getStartDate(p_handle, &date);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(35796., date);
+    EXPECT_EQ(35796., date);
 }
 
 TEST_F(OutputapiTest, getTimesTest) {
-	int time = -1;
+    int time = -1;
 
-	error = SMO_getTimes(p_handle, reportStep, &time);
-	ASSERT_EQ(0, error);
+    error = SMO_getTimes(p_handle, reportStep, &time);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(3600, time);
+    EXPECT_EQ(3600, time);
 
-	error = SMO_getTimes(p_handle, numPeriods, &time);
-	ASSERT_EQ(0, error);
+    error = SMO_getTimes(p_handle, numPeriods, &time);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(36, time);
+    EXPECT_EQ(36, time);
 }
 
 TEST_F(OutputapiTest, getElementNameTest) {
-	char* c_array = NULL;
-	int index = 1;
+    char* c_array = NULL;
+    int index = 1;
 
-	error = SMO_getElementName(p_handle, node, index, &c_array, &array_dim);
-	ASSERT_EQ(0, error);
+    error = SMO_getElementName(p_handle, node, index, &c_array, &array_dim);
+    ASSERT_EQ(0, error);
 
-	EXPECT_STREQ("10", c_array);
+    EXPECT_STREQ("10", c_array);
 
-	SMO_free((void**)&c_array);
+    SMO_free((void**)&c_array);
 
 }
 
@@ -170,93 +170,93 @@ TEST_F(OutputapiTest, getElementNameTest) {
 //}
 
 TEST_F(OutputapiTest, getSubcatchResultTest) {
-	float ref_array[10] = {0.5,
-			               0.0,
-						   0.0,
-						   0.125,
-						   1.2438242,
-						   0.0,
-						   0.0,
-						   0.0,
-						   33.481991,
-	                       6.6963983};
+    float ref_array[10] = {0.5,
+            0.0,
+            0.0,
+            0.125,
+            1.2438242,
+            0.0,
+            0.0,
+            0.0,
+            33.481991,
+            6.6963983};
 
-	error = SMO_getSubcatchResult(p_handle, 1, 1, &array, &array_dim);
-	ASSERT_EQ(0, error);
+    error = SMO_getSubcatchResult(p_handle, 1, 1, &array, &array_dim);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(10, array_dim);
-	for (int i = 0; i < array_dim; i++)
-		EXPECT_FLOAT_EQ(ref_array[i], array[i]);
+    EXPECT_EQ(10, array_dim);
+    for (int i = 0; i < array_dim; i++)
+        EXPECT_FLOAT_EQ(ref_array[i], array[i]);
 }
 
 TEST_F(OutputapiTest, getNodeResultTest) {
-	float ref_array[8] = {0.296234,
-			               995.296204,
-			               0.0,
-						   1.302650,
-						   1.302650,
-			               0.0,
-						   15.361463,
-						   3.072293};
+    float ref_array[8] = {0.296234,
+            995.296204,
+            0.0,
+            1.302650,
+            1.302650,
+            0.0,
+            15.361463,
+            3.072293};
 
-	error = SMO_getNodeResult(p_handle, 2, 2, &array, &array_dim);
-	ASSERT_EQ(0, error);
+    error = SMO_getNodeResult(p_handle, 2, 2, &array, &array_dim);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(8, array_dim);
-	for (int i = 0; i < array_dim; i++)
-		EXPECT_FLOAT_EQ(ref_array[i], array[i]);
+    EXPECT_EQ(8, array_dim);
+    for (int i = 0; i < array_dim; i++)
+        EXPECT_FLOAT_EQ(ref_array[i], array[i]);
 }
 
 TEST_F(OutputapiTest, getLinkResultTest) {
-	float ref_array[7] = {4.631762,
-                          1.0,
-                          5.8973422,
-                          314.15927,
-			              1.0,
-						  19.070757,
-						  3.8141515};
+    float ref_array[7] = {4.631762,
+            1.0,
+            5.8973422,
+            314.15927,
+            1.0,
+            19.070757,
+            3.8141515};
 
-	error = SMO_getLinkResult(p_handle, 3, 3, &array, &array_dim);
-	ASSERT_EQ(0, error);
+    error = SMO_getLinkResult(p_handle, 3, 3, &array, &array_dim);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(7, array_dim);
-	for (int i = 0; i < array_dim; i++)
-		EXPECT_FLOAT_EQ(ref_array[i], array[i]);
+    EXPECT_EQ(7, array_dim);
+    for (int i = 0; i < array_dim; i++)
+        EXPECT_FLOAT_EQ(ref_array[i], array[i]);
 }
 
 TEST_F(OutputapiTest, getSystemResultTest) {
-	float ref_array[14] = {70.0,
-			               0.1,
-			               0.0,
-						   0.19042271,
-						   14.172027,
-			               0.0,
-			               0.0,
-			               0.0,
-			               0.0,
-						   14.172027,
-						   0.55517411,
-						   13.622702,
-						   2913.0793,
-						   0.0};
+    float ref_array[14] = {70.0,
+            0.1,
+            0.0,
+            0.19042271,
+            14.172027,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            14.172027,
+            0.55517411,
+            13.622702,
+            2913.0793,
+            0.0};
 
-	error = SMO_getSystemResult(p_handle, 4, 4, &array, &array_dim);
-	ASSERT_EQ(0, error);
+    error = SMO_getSystemResult(p_handle, 4, 4, &array, &array_dim);
+    ASSERT_EQ(0, error);
 
-	EXPECT_EQ(14, array_dim);
-	for (int i = 0; i < array_dim; i++)
-		EXPECT_FLOAT_EQ(ref_array[i], array[i]);
+    EXPECT_EQ(14, array_dim);
+    for (int i = 0; i < array_dim; i++)
+        EXPECT_FLOAT_EQ(ref_array[i], array[i]);
 }
 
 }
 
 
 GTEST_API_ int main(int argc, char **argv) {
-	OutputapiTest::SetUpTestCase();
-	printf("Running main() from gtest_main.cc\n");
-	testing::InitGoogleTest(&argc, argv);
+    OutputapiTest::SetUpTestCase();
+    printf("Running main() from gtest_main.cc\n");
+    testing::InitGoogleTest(&argc, argv);
 
-	return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 }
 
 
