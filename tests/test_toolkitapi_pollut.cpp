78572434a855e2ca46d0ce18f25987487923b996
Katherine Ratliff <ratliff.katherine@epa.gov>
2018-07-24 09:12:11 -0400

    add CPonded, another time step check to pollutant api unit test

--- tests/test_toolkitapi_pollut.cpp
+++ tests/test_toolkitapi_pollut.cpp
@@ -20,7 +20,8 @@ BOOST_AUTO_TEST_SUITE(test_toolkitapi_pollut)
 BOOST_FIXTURE_TEST_CASE(get_pollut_values, FixtureBeforeStep){
     int error, step_ind;
     int subc_ind;
-    double* pollut_array;
+    double* buildup_array;
+    double* ponded_array;
     double elapsedTime = 0.0;
     // Pollutant IDs
     int TSS = 0;
@@ -38,11 +39,31 @@ BOOST_FIXTURE_TEST_CASE(get_pollut_values, FixtureBeforeStep){
         if (step_ind == 360) // (Jan 1, 1998 6:00am)
         {
             // buildup
-            error = swmm_getSubcatchPollut(subc_ind, SM_BUILDUP, &pollut_array);
+            error = swmm_getSubcatchPollut(subc_ind, SM_BUILDUP, &buildup_array);
             BOOST_REQUIRE(error == ERR_NONE);
-            BOOST_CHECK_SMALL(pollut_array[TSS] - 31.906912, 0.0001);
-            BOOST_CHECK_SMALL(pollut_array[Lead] - 0.0, 0.0001);
-            
+            BOOST_CHECK_SMALL(buildup_array[TSS] - 31.906912, 0.0001);
+            BOOST_CHECK_SMALL(buildup_array[Lead] - 0.0, 0.0001);
+
+            // ponded concentration
+            error = swmm_getSubcatchPollut(subc_ind, SM_CPONDED, &ponded_array);
+            BOOST_REQUIRE(error == ERR_NONE);
+            BOOST_CHECK_SMALL(ponded_array[TSS] - 0.0, 0.0001);
+            BOOST_CHECK_SMALL(ponded_array[Lead] - 0.0, 0.0001);
+        }
+
+        if (step_ind == 720) // (Jan 1, 1998 12:00pm)
+        {
+            // buildup
+            error = swmm_getSubcatchPollut(subc_ind, SM_BUILDUP, &buildup_array);
+            BOOST_REQUIRE(error == ERR_NONE);
+            BOOST_CHECK_SMALL(buildup_array[TSS] - 32.354460, 0.0001);
+            BOOST_CHECK_SMALL(buildup_array[Lead] - 0.0, 0.0001);
+
+            // ponded concentration
+            error = swmm_getSubcatchPollut(subc_ind, SM_CPONDED, &ponded_array);
+            BOOST_REQUIRE(error == ERR_NONE);
+            BOOST_CHECK_SMALL(ponded_array[TSS] - 0.0, 0.0001);
+            BOOST_CHECK_SMALL(ponded_array[Lead] - 0.0, 0.0001);
         }
 
         // Route Model Forward
@@ -51,7 +72,8 @@ BOOST_FIXTURE_TEST_CASE(get_pollut_values, FixtureBeforeStep){
     }while (elapsedTime != 0 && !error);
     BOOST_REQUIRE(error == ERR_NONE);
 
-    freeArray((void**) &pollut_array);
+    freeArray((void**) &buildup_array);
+    freeArray((void**) &ponded_array);
 
     swmm_end();
 }
