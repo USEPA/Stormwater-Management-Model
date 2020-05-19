71812320354c6fd62ab9fc15edc0065bc190652d
Michael Tryby <tryby.michael@epa.gov>
2020-05-19 12:31:04 -0400

    Update make.cmd

--- tools/make.cmd
+++ tools/make.cmd
@@ -104,7 +104,7 @@ for /F "tokens=*" %%f in ( 'findstr CMAKE_SHARED_LINKER_FLAGS:STRING %BUILD_HOME
 if not defined PLATFORM ( echo "ERROR: PLATFORM could not be determined" & exit /B 1 )
 
 :: GitHub Actions
-if exists %CI% (
+if exist %CI% (
     echo ::set-env name=PLATFORM::%PLATFORM%
 )
 
