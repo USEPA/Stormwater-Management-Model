214d7cccdf95473b4dd3928a8ec8114a10f4acc3
Jenn Wu <jwu@emnet.net>
2020-10-21 16:20:53 -0400

    Release version 5.2.0.dev7

--- src/consts.h
+++ src/consts.h
@@ -19,7 +19,7 @@
 #define   VERSION            52000          // Eventually will be deprecated.
 #define   SEMVERSION_MAJOR   "5"            // Major Semantic Version
 #define   SEMVERSION_MINOR   "2"            // Minor Semantic Version
-#define   SEMVERSION_PATCH   "0.dev6"      // Patch Semantic Version
+#define   SEMVERSION_PATCH   "0.dev7"      // Patch Semantic Version
 #define   SEMVERSION_LEN     20             // Version String Len
 
 #define   MAGICNUMBER        516114522
