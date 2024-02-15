cd5120d61e82137721443353980f94890b60b9e3
Caleb <calebgh@gmail.com>
2024-02-15 16:21:30 -0500

    WIP addressing #162, #150

---
+++ tests/solver/test_solver_hotstart.cpp
@@ -0,0 +1,24 @@
+/*
+ *   test_solver_hotstart.cpp
+ *
+ *   Created: 02/01/2024
+ *   Updated: 02/01/2024
+ * 
+ *   Unit testing for SWMM solver api hotstart saving.
+ */
+
+
+#define BOOST_TEST_MODULE "solver hotstart"
+#include <boost/test/included/unit_test.hpp>
+
+
+#include "swmm5.h"
+
+BOOST_AUTO_TEST_SUITE(test_solver_hotstart)
+
+BOOST_AUTO_TEST_CASE(test_getVersion) {
+
+	BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
