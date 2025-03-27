fb4f408275d1aab3f7c561a02f18322d68c8bf85
Caleb <calebgh@gmail.com>
2025-03-27 15:53:53 -0400

    Work in progress

--- python/epaswmm/__init__.py
+++ python/epaswmm/__init__.py
@@ -9,26 +9,30 @@ import platform
 import sys
 import importlib.metadata
 
-if platform.system() == "Windows":
-    lib_dir = os.path.join(sys.prefix, "bin")
-    if hasattr(os, "add_dll_directory"):
-        conda_exists = os.path.exists(os.path.join(sys.prefix, "conda-meta"))
-        if conda_exists:
-            os.environ["CONDA_DLL_SEARCH_MODIFICATION_ENABLE"] = "1"
-        os.add_dll_directory(lib_dir)
-    else:
-        os.environ["PATH"] = lib_dir + ";" + os.environ["PATH"]
 
-elif platform.system() == "Linux":
-    lib_dir = os.path.join(sys.prefix, "lib")
-    os.environ["LD_LIBRARY_PATH"] = lib_dir + ":" + os.environ.get("LD_LIBRARY_PATH", "")
+try:
+    from epaswmm import *
+except ImportError:
+    if platform.system() == "Windows":
+        lib_dir = os.path.join(sys.prefix, "bin")
+        if hasattr(os, "add_dll_directory"):
+            conda_exists = os.path.exists(os.path.join(sys.prefix, "conda-meta"))
+            if conda_exists:
+                os.environ["CONDA_DLL_SEARCH_MODIFICATION_ENABLE"] = "1"
+            os.add_dll_directory(lib_dir)
+        else:
+            os.environ["PATH"] = lib_dir + ";" + os.environ["PATH"]
 
-elif platform.system() == "Darwin":  # macOS
-    lib_dir = os.path.join(sys.prefix, "lib")
-    os.environ["DYLD_LIBRARY_PATH"] = lib_dir + ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
+    elif platform.system() == "Linux":
+        lib_dir = os.path.join(sys.prefix, "lib")
+        os.environ["LD_LIBRARY_PATH"] = lib_dir + ":" + os.environ.get("LD_LIBRARY_PATH", "")
 
-    lib_dir = os.path.join(sys.prefix, "bin")
-    os.environ["DYLD_LIBRARY_PATH"] = lib_dir + ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
+    elif platform.system() == "Darwin":  # macOS
+        lib_dir = os.path.join(sys.prefix, "lib")
+        os.environ["DYLD_LIBRARY_PATH"] = lib_dir + ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
+
+        lib_dir = os.path.join(sys.prefix, "bin")
+        os.environ["DYLD_LIBRARY_PATH"] = lib_dir + ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
 
 __version__ = importlib.metadata.version('epaswmm')
 
